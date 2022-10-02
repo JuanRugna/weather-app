10af89b25148045fd1c8bf74acbe7d68cf7356df
JuanRugna <juanrugna@hotmail.com>
2022-10-02 20:41:08 +0200

    Creación de repositorio

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3e3b142f5cmshaca18752fad6616p125991jsn79dffd4553b5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+}
+
+  
+
+export const GEO_API_URL= 'https://wft-geo-db.p.rapidapi.com/v1/geo'
